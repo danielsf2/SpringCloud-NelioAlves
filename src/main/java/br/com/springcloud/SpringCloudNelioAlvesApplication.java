07c3f386058de3d430649741ff13832c00d87b9f
Daniel Soares <danielsf850@gmail.com>
2022-02-15 17:00:56 -0300

    Projeto Criado

---
+++ src/main/java/br/com/springcloud/SpringCloudNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package br.com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNelioAlvesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNelioAlvesApplication.class, args);
+    }
+
+}
